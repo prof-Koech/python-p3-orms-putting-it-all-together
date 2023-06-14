d223aa45bd79a7aa5764d58ff7f9d350166d0666
Kennedy <kiplangatkennedy019@gmail.com>
2023-06-14 12:25:00 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -5,4 +5,100 @@ CURSOR = CONN.cursor()
 
 class Dog:
     
-    pass
+    all = []
+    
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            );
+        """
+
+        cls.all = CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS dogs;
+        """
+
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql = """
+            INSERT INTO dogs (name, breed) VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.breed))
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM dogs").fetchone()[0]
+
+    @classmethod
+    def create(cls, name, breed):
+        dog = Dog(name, breed)
+        dog.save()
+
+        return dog
+
+    @classmethod
+    def new_from_db(cls, row):
+        dog = cls(row[1], row[2])
+        dog.id = row[0]
+        return dog
+
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT * FROM dogs
+        """
+
+        dogs = CURSOR.execute(sql).fetchall()
+        all = [cls.new_from_db(dog) for dog in dogs]
+        return all
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT *
+            FROM dogs
+            WHERE name = ?
+        """
+
+        dog = CURSOR.execute(sql, (name,)).fetchone()
+        if dog:
+            return cls.new_from_db(dog)
+
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT *
+            FROM dogs
+            WHERE id = ?
+        """
+
+        dog = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.new_from_db(dog)
+
+    @classmethod
+    def find_or_create_by(cls, name, breed):
+        dog = cls.find_by_name(name)
+        
+        if not dog:
+            dog = cls.create(name, breed)
+            
+        return dog
+
+    def update(self):
+        sql = """
+            UPDATE dogs
+            SET name = ?, breed = ?
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.name, self.breed, self.id))
